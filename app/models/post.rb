246b50b3aef5c4a176c76a1331b3f470b123e41e
julesow <souleymanesow@gmail.com>
2019-01-21 18:24:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  belongs_to :author
+  validates :title,presence:true ,blank:false
+  validates :category ,inclusion: { in: ["Fiction","Non-Fiction"] }
+  validates :content,length:{minimum:100}
+  # def fiction?
+  #   fiction
+  # end
+  #
+  # def nonfiction?
+  #   !fiction
+  # end
 end
